6be1c50d5ea580adb0829519569fa692d54c2a1c
heywon0909 <hw731911@gmail.com>
2026-01-29 17:50:42 +0900

    feat: post schema type 생성

---
+++ schemaTypes/postType.ts
@@ -0,0 +1,58 @@
+import {defineArrayMember, defineField, defineType, validation} from 'sanity'
+
+export const postType = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'author',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'likes',
+      type: 'number',
+      initialValue: 0,
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'comments',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'object',
+          fields: [
+            defineField({name: 'name', type: 'string', validation: (rule) => rule.required()}),
+            defineField({name: 'body', type: 'string', validation: (rule) => rule.required()}),
+          ],
+        }),
+      ],
+    }),
+  ],
+})
