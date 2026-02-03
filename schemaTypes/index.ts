4bddc53d71c9c7fb2087aad56613e6e8af31cabf
heywon0909 <hw731911@gmail.com>
2026-02-03 09:59:29 +0900

    feat: user type 추가

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,4 @@
 import {postType} from './postType'
+import {userType} from './userType'
 
-export const schemaTypes = [postType]
+export const schemaTypes = [postType, userType]
---
+++ schemaTypes/userType.ts
@@ -0,0 +1,62 @@
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const userType = defineType({
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'username',
+      title: 'Username',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'reference',
+          to: [{type: 'user'}],
+        }),
+      ],
+    }),
+    defineField({
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'reference',
+          to: [{type: 'user'}],
+        }),
+      ],
+    }),
+    defineField({
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'reference',
+          to: [{type: 'post'}],
+        }),
+      ],
+    }),
+  ],
+})
